dffc2b96792b52a6b7e6a9cf9bffda3464ca98a4
Sean Gregory <seanchristophergregory@gmail.com>
2020-04-19 09:07:07 -0400

    passing specs

--- test/dossier/handler_test.clj
+++ test/dossier/handler_test.clj
@@ -1,6 +1,7 @@
 (ns dossier.handler-test
   (:require [clojure.test :refer :all]
             [dossier.migrate :refer :all]
+            [cheshire.core :refer :all]
             [ring.mock.request :as mock]
             [dossier.handler :refer :all
              ]))
@@ -16,7 +17,7 @@
 (use-fixtures :once reset-database)
 (use-fixtures :each clear-database)
 
-(deftest test-app
+(deftest test-handler
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
       (is (= (:status response) 404))))
@@ -24,4 +25,14 @@
   (testing "unauthorized route with no session"
     (let [response (app (mock/request :get "/user"))]
       (is (= (:status response) 401))
-      (is (= (:body response) "Unauthorized" )))))
+      (is (= (:body response) "Unauthorized" ))))
+
+  (testing "duplicate registration"
+    (let [request-body  {:email-address "bob@xyz.com", :password "hello" :password-confirmation "hello"}
+          response1 (app (-> (mock/request :post "/auth")
+                            (mock/json-body request-body)))
+          response2 (app (-> (mock/request :post "/auth")
+                            (mock/json-body request-body)))]
+      (is (= (:status response1) 200))
+      (is (= (:status response2) 400))
+      (is (= (get (parse-string (:body response2) true) :email-address) ["Email already exists"])))))
